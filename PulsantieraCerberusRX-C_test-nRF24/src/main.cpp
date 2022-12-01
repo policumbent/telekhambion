8a4ceca275aa3198b4c1f8afe23e8402e9cc5811
fraimparato <fra.imparato20@gmail.com>
2022-12-02 00:38:32 +0100

    first test: - basic nRF24 management - button-driven TX (example of shift-up, shift-down, radio)
    Used UNO as TX, NANO as RX

--- PulsantieraCerberusRX-C_test-nRF24/src/main.cpp
+++ PulsantieraCerberusRX-C_test-nRF24/src/main.cpp
@@ -3,10 +3,41 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio(7,8);  //CE=7, CSN=8
+const byte address[6] = "00001";
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);                 // start baud rate for serial monitor
+  
+  // setup nRF24L01 as RX
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
+uint8_t sig = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(radio.available()) {
+    radio.read(&sig, sizeof(sig));
+    
+    switch (sig) {
+    case 255:
+      Serial.print("Up-shift!\n");
+      break;
+
+    case 1:
+      Serial.print("Down-shift!\n");
+      break;
+
+    case 127:
+      Serial.print("Radio!\n");
+      break;
+    
+    default:
+      Serial.print("Corrupted signal!");
+      break;
+    }
+  }
 }
--- PulsantieraCerberusTX_test-nRF24/src/main.cpp
+++ PulsantieraCerberusTX_test-nRF24/src/main.cpp
@@ -3,10 +3,44 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+const uint8_t up_shift_sig = 255;
+const uint8_t down_shift_sig = 1;
+const uint8_t radio_sig = 127;
+
+RF24 radio(7,8);  //CE=7, CSN=8
+const byte address[6] = "00001";
+
+int shift_up = 0;
+int shift_down = 0;
+int radio_tx = 0;
+
+/*
+ *  b_su: button shift up;
+ *  b_sd: button shift down;
+ *  b_rd: button radio;
+ */
+int b_su = 2, b_sd = 3, b_rd = 4;
+
 void setup() {
-  // put your setup code here, to run once:
+  // input pins (buttons) init
+  pinMode(b_su, INPUT);
+  pinMode(b_sd, INPUT);
+  pinMode(b_rd, INPUT);
+
+  // nRF24 init
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  shift_up = digitalRead(b_su);
+  shift_down = digitalRead(b_sd);
+  radio_tx = digitalRead(b_rd);
+
+  if(shift_up) radio.write(&up_shift_sig, sizeof(uint8_t));
+  else if(shift_down) radio.write(&down_shift_sig, sizeof(uint8_t));
+
+  if(radio_tx) radio.write(&radio_sig, sizeof(uint8_t));
 }
